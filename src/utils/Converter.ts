6ac7a1e404c79760610b4581447b7a5759ade462
Shafira Naya <shfrnaya@gmail.com>
2022-02-27 19:16:40 +0700

    fix: change position in converter for square & rectangle

--- src/utils/Converter.ts
+++ src/utils/Converter.ts
@@ -14,29 +14,20 @@ export function convertJsonToObject(
   switch (jsonData._model) {
     case "LINE":
       object = new Line(jsonData._id, gl, program);
-      position = jsonData._position;
       break;
     case "TRIANGLE":
       object = new Triangle(jsonData._id, gl, program);
-      position = jsonData._position;
       break;
     // For rectangle and square, the number of _position is 2
     case "RECTANGLE":
       object = new Rectangle(jsonData._id, gl, program);
-      position = jsonData._position
-        .slice(0, 1)
-        .concat(jsonData._position.slice(2, 3));
       break;
     case "SQUARE":
       object = new Square(jsonData._id, gl, program);
-      position = jsonData._position
-        .slice(0, 1)
-        .concat(jsonData._position.slice(2, 3));
       break;
     case "POLYGON":
       // TODO polygon
       // webGLObject = new Polygon(jsonData.id,gl,program);
-      // _position = jsonData._position;
       break;
     default:
       break;
